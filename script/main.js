0a380b5b0d7a18e0ee94e16a4eac78361dec6d82
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-02-14 22:20:56 -0300

    Mostrando as tarefas na tela
    ✨Novidades:
    - Ver as tarefas na tela
    - Visual do form para cadastrar tarefa
    
    🔧 Testes:
    - visual da lista para filtrar as lista pelo dia
    
    💡Ideias Futuras:
    - dar para ver e ocultar os detalhes das tarefas
    
    🚧Objetivos em progresso:
    - Separar as tarefas por dia
    - Editar a tarefa completamente
    - Excluir tarefa especifica
    - Excluir todas as tarefas

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 // Pegando os itens do form e o form
 let form = document.querySelector("form")
 let inputDiaDaTarefa = document.getElementById("diaDaTarefa")
+let inputTituloDaTarefa = document.getElementById("tituloDaTarefa")
 let selectSelecaoHorario = document.getElementById("selecaoHorario")
 let textAreaDetalhesDaTarefa = document.getElementById("detalhesDaTarefa")
 let botaoSubmit = document.getElementById("botaoSubmit")
@@ -13,20 +14,43 @@ form.addEventListener("submit",function criarTarefa(event){
     event.preventDefault()
     let tarefa ={
         dia:inputDiaDaTarefa.value,
+        titulo:inputTituloDaTarefa.value,
         horario:selectSelecaoHorario.value,
         detalhes:textAreaDetalhesDaTarefa.value,
     }
 
-    localStorage.setItem("tarefas",JSON.stringify(tarefa))
+    adicionarTarefa(tarefa)
     mostrar()
 })
+
+// Funções para adicionar a tarefa
+function adicionarTarefa(tarefa){
+    let tarefas = pegarTarefasLocalStorage();
+    tarefas.push(tarefa)
+    adicionarClienteLocalStorage(tarefas)
+}
+
+function adicionarClienteLocalStorage(tarefas){
+    return localStorage.setItem("tarefas",JSON.stringify(tarefas))
+}
 
 // Função para mostrar a lista
 function pegarTarefasLocalStorage(){
     return JSON.parse(localStorage.getItem('tarefas')) ?? []
 }
 
-function mostrar(){
-    let verTarefas = pegarTarefasLocalStorage()
+async function mostrar(){
+    let verTarefas = await pegarTarefasLocalStorage()
     console.log(verTarefas)
+    verTarefas.forEach(tarefa => {
+        listaDasTarefas.innerHTML += `
+        <div>
+            <h2>Dia:${tarefa.dia}</h2>
+            <h3>${tarefa.titulo}</h3>
+            <p>Horario:${tarefa.horario}</p>
+            <p>Sobre:${tarefa.detalhes}</p>
+        </div>`
+    });
 }
+
+mostrar()
