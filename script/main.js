0ed42f300def46f7a49f87906e4f9dc5fe45fe91
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-02-15 23:19:52 -0300

    Filtro dos dias
    ✨Novidades:
    - Filtrar os dias das tarefas
    
    🔧 Testes:
    - Mostrar a tarefa na aba do dia correspondente
    
    🚧Objetivos em progresso:
    - Mostrar as tarefas por dia
    - Editar a tarefa completamente
    - Excluir tarefa especifica
    - Excluir todas as tarefas

--- script/main.js
+++ script/main.js
@@ -10,13 +10,13 @@ let botaoSubmit = document.getElementById("botaoSubmit")
 let listaDasTarefas = document.getElementById("listaDasTarefas")
 
 // Anulando o submit do form
-form.addEventListener("submit",function criarTarefa(event){
+form.addEventListener("submit", function criarTarefa(event) {
     event.preventDefault()
-    let tarefa ={
-        dia:inputDiaDaTarefa.value,
-        titulo:inputTituloDaTarefa.value,
-        horario:selectSelecaoHorario.value,
-        detalhes:textAreaDetalhesDaTarefa.value,
+    let tarefa = {
+        dia: inputDiaDaTarefa.value,
+        titulo: inputTituloDaTarefa.value,
+        horario: selectSelecaoHorario.value,
+        detalhes: textAreaDetalhesDaTarefa.value,
     }
 
     adicionarTarefa(tarefa)
@@ -24,33 +24,105 @@ form.addEventListener("submit",function criarTarefa(event){
 })
 
 // Funções para adicionar a tarefa
-function adicionarTarefa(tarefa){
+function adicionarTarefa(tarefa) {
     let tarefas = pegarTarefasLocalStorage();
     tarefas.push(tarefa)
     adicionarClienteLocalStorage(tarefas)
 }
 
-function adicionarClienteLocalStorage(tarefas){
-    return localStorage.setItem("tarefas",JSON.stringify(tarefas))
+function adicionarClienteLocalStorage(tarefas) {
+    return localStorage.setItem("tarefas", JSON.stringify(tarefas))
 }
 
 // Função para mostrar a lista
-function pegarTarefasLocalStorage(){
+function pegarTarefasLocalStorage() {
     return JSON.parse(localStorage.getItem('tarefas')) ?? []
 }
 
-async function mostrar(){
+async function mostrar(diaDaqui) {
     let verTarefas = await pegarTarefasLocalStorage()
-    console.log(verTarefas)
-    verTarefas.forEach(tarefa => {
-        listaDasTarefas.innerHTML += `
-        <div>
-            <h2>Dia:${tarefa.dia}</h2>
-            <h3>${tarefa.titulo}</h3>
-            <p>Horario:${tarefa.horario}</p>
-            <p>Sobre:${tarefa.detalhes}</p>
-        </div>`
-    });
+
+    // verTarefas.forEach(tarefa => {
+    //     listaDasTarefas.innerHTML += `
+    //     <div>
+    //         <h2>Dia:${tarefa.dia[0]}</h2>
+    //         <h3>${tarefa.titulo[0]}</h3>
+    //         <p>Horario:${tarefa.horario[0]}</p>
+    //         <p>Sobre:${tarefa.detalhes[0]}</p>
+    //     </div>`
+    // });
 }
 
-mostrar()
+// filtro por dia
+
+async function filtrarDias() {
+    let verTarefas = await pegarTarefasLocalStorage()
+    let diasAdicionados = []
+    verTarefas.forEach(tarefa => {
+        diasAdicionados.push(tarefa.dia)
+    })
+    let diasFiltrados = [...new Set(diasAdicionados)]
+    return await diasFiltrados.sort((a, b) => a - b)
+}
+
+let listaDeDias = document.getElementById("listaDeDias")
+let abasNoMenu = []
+async function adicionarDiasAoMenu() {
+    let diasCriados = await filtrarDias()
+
+    listaDeDias.innerHTML = ``
+
+    await diasCriados.forEach(dia => {listaDeDias.innerHTML += `
+    <menuitem class="diaNoMenu" data-diaNoMenu="${dia}">Dia ${dia}</menuitem>`
+    })
+    abasNoMenu = await document.querySelectorAll("[data-diaNoMenu]")
+    abasNoMenu.forEach(diaNoMenu=>{
+        diaNoMenu.addEventListener("click",()=>{
+            let valor = diaNoMenu.getAttribute("data-diaNoMenu")
+            esconderAbas()
+            destivarAbas(valor)
+            ativarSection(valor)
+            ativarAba(diaNoMenu)
+        })
+    })
+}
+
+adicionarDiasAoMenu()
+
+let paginas = document.getElementById("paginas")
+
+async function criarSecaoPorDia(){
+    let verTarefas = pegarTarefasLocalStorage()
+    let diasCriados = await filtrarDias()
+    paginas.innerHTML = ``
+    diasCriados.forEach(dia =>{
+        paginas.innerHTML+=`
+        <section class="tarefa ativa" data-pagina="${dia}">${mostrar(dia)}</section>`
+    })
+}
+
+criarSecaoPorDia()
+
+// Mostrar tarefas por abas
+
+function esconderAbas(){
+    const paginas = document.querySelectorAll("[data-pagina]")
+
+    paginas.forEach(pagina => pagina.classList.add('hide'))
+}
+
+function destivarAbas() {
+    const paginas = document.querySelectorAll(`[data-pagina]`)
+    paginas.forEach(secao => secao.classList.remove("ativa"))
+}
+
+const ativarSection = (valor) => {
+    const pagina= document.querySelector(`[data-pagina="${valor}"]`)
+
+    pagina.classList.remove("hide")
+    pagina.classList.add("ativa")
+}
+
+function ativarAba(aba){
+    aba.classList.add("ativa")
+}
